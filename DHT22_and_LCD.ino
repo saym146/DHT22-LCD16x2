dc4d08fa556557d1029e9585c769e920a72f631e
SayantaN <29336612+saym146@users.noreply.github.com>
2025-11-22 11:41:19 +0530

    Enhance DHT22 and LCD integration with WiFi and MQTT
    Refactor DHT22 and LCD setup, add WiFi and MQTT functionality.

--- DHT22_and_LCD.ino
+++ DHT22_and_LCD.ino
@@ -1,70 +1,176 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
 #include <LiquidCrystal.h>
-#include <DHT.h>;
 
-//Constants
-#define DHTPIN 7     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+// -------------------------
+// WIFI + MQTT SETTINGS
+// -------------------------
+const char* WIFI_SSID     = "SSID";
+const char* WIFI_PASSWORD = "wifiPass";
 
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+const char* MQTT_SERVER   = "broker Ip/Domain";
+const uint16_t MQTT_PORT     = 1883;
+const char* MQTT_USER     = "USER";
+const char* MQTT_PASS     = "PASS";
 
-// Setup the LiquidCrystal library with the pin numbers we have
-// physically connected the module to.
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const char* TOPIC_TEMP = "home/room1/temperature";
+const char* TOPIC_HUM  = "home/room1/humidity";
 
-//Variables
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-float maxHum;
-float minHum;
-float maxTemp;
-float minTemp;
-int lastUpdate = 0;  
-int currentIndex = 0; 
+// -------------------------
+// DHT22 CONFIG
+// -------------------------
+#define DHTPIN 10
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
+// -------------------------
+// LCD CONFIG
+// -------------------------
+LiquidCrystal lcd(D1, D2, D3, D4, D5, D6);
 
-void setup() {  
-  // Setup the number of columns and rows that are available on the LCD. 
+// -------------------------
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// status variables
+bool wifiOK = false;
+bool mqttOK = false;
+
+// -------------------------
+// Tick & Cross CUSTOM CHARS
+// -------------------------
+byte tickChar[8] = {
+  B00000,
+  B00001,
+  B00011,
+  B10110,
+  B11100,
+  B01000,
+  B00000,
+  B00000
+};
+
+byte crossChar[8] = {
+  B00000,
+  B10001,
+  B01010,
+  B00100,
+  B01010,
+  B10001,
+  B00000,
+  B00000
+};
+
+// -------------------------
+void setupWiFiSafe() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  unsigned long startAttempt = millis();
+  wifiOK = false;
+
+  // Try 3 seconds max (non-blocking)
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttempt < 3000) {
+    delay(200);
+  }
+
+  wifiOK = (WiFi.status() == WL_CONNECTED);
+}
+
+// -------------------------
+void setupMQTTSafe() {
+  if (!wifiOK) {
+    mqttOK = false;
+    return;
+  }
+
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+
+  // Try once (non-blocking, no loop)
+  mqttOK = client.connect("testClient", MQTT_USER, MQTT_PASS);
+}
+
+// -------------------------
+void setup() {
+  Serial.begin(115200);
   lcd.begin(16, 2);
-  //Start DHT22 
+
+  lcd.createChar(0, tickChar);
+  lcd.createChar(1, crossChar);
+
   dht.begin();
+
+  setupWiFiSafe();
+  setupMQTTSafe();
 }
 
-void loop() {  
-      
-    temp =  GetTemperature();
-    hum =   GetHumidity();
+// -------------------------
+void loop() {
 
-    if(isnan(temp) != 1 && isnan(hum) != 1)
-    {
-      maxTemp = max(maxTemp,temp);
-      minTemp = minTemp == 0? maxTemp : min(minTemp,temp);
-      
-      maxHum = max(maxHum,hum);
-      minHum = minHum == 0? maxHum : min(minHum,hum);
-      
-      lcd.setCursor(0, 0);
-      lcd.print("T:"+String(temp,1)+" "+String(maxTemp,1)+"/"+String(minTemp,1));
-      lcd.setCursor(0, 1);
-      lcd.print("H:"+String(hum,1)+" "+String(maxHum,1)+"/"+String(minHum,1));
+  // reconnect attempts (SAFE, no blocking)
+  if (WiFi.status() == WL_CONNECTED) {
+    wifiOK = true;
+  } else {
+    wifiOK = false;
+    setupWiFiSafe();
+  }
+
+  if (wifiOK) {
+    if (!client.connected()) {
+      setupMQTTSafe();
+    } else {
+      mqttOK = true;
     }
-    else{
-      lcd.setCursor(2,0);
-      lcd.print("No DHT Found!!");
-      //For moving sentence
-//      for(int i=15;i>=0;i--){
-//        lcd.setCursor(i,0);
-//        lcd.print("No DHT Found");
-//        delay(500);
-//      }
-    }
-}
+  } else {
+    mqttOK = false;
+  }
 
-float GetHumidity()
-{
-  return dht.readHumidity();
-}
+  client.loop();
 
-float GetTemperature()
-{
-  return dht.readTemperature();
+  float temp = dht.readTemperature();
+  float hum  = dht.readHumidity();
+
+  lcd.clear();
+
+  // -------------------------
+  // Line 1 - Temperature
+  // -------------------------
+  lcd.setCursor(0, 0);
+  lcd.print("T: ");
+  lcd.print(temp, 2);
+  lcd.print((char)223);
+  lcd.print("C");
+
+  // -------------------------
+  // Line 2 - Humidity + status
+  // -------------------------
+  lcd.setCursor(0, 1);
+  lcd.print("H: ");
+  lcd.print(hum, 2);
+  lcd.print("% ");
+
+  // WiFi Status
+  lcd.setCursor(10, 1);
+  lcd.print("W:");
+  lcd.write(wifiOK ? 0 : 1);
+
+  // MQTT Status
+  lcd.setCursor(13, 1);
+  lcd.print("M:");
+  lcd.write(mqttOK ? 0 : 1);
+
+  // -------------------------
+  // MQTT publishing IF AVAILABLE ONLY
+  // -------------------------
+  if (wifiOK && mqttOK) {
+    char tStr[10];
+    char hStr[10];
+    dtostrf(temp, 4, 2, tStr);
+    dtostrf(hum, 4, 2, hStr);
+
+    client.publish(TOPIC_TEMP, tStr);
+    client.publish(TOPIC_HUM, hStr);
+  }
+
+  delay(3000);
 }
